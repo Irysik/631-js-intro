f25e87c3feaad53de569d85ec8acb342f330e65d
Irysik <89611587+Irysik@users.noreply.github.com>
2021-09-21 14:07:46 +0300

    Update 631-01.js

--- 631-01.js
+++ 631-01.js
@@ -4,3 +4,11 @@ function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   // code goes here
   return laLigaGoals+copaDelReyGoals+championsLeagueGoals;
 }
+
+
+https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+
+function move (position, roll) {
+  // return the new position
+  return position+roll*2
+}
