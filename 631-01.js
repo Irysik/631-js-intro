0f928aa34ca7bbbcae33a695d92a143c095546de
Irysik <89611587+Irysik@users.noreply.github.com>
2021-09-21 14:05:10 +0300

    Update 631-01.js

--- 631-01.js
+++ 631-01.js
@@ -1,1 +1,6 @@
+https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
 
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  // code goes here
+  return laLigaGoals+copaDelReyGoals+championsLeagueGoals;
+}
