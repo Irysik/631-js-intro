cc280767dc061677a17696e2fc0c70546e1a9bb5
Irysik <89611587+Irysik@users.noreply.github.com>
2021-09-21 14:18:29 +0300

    Update 631-01.js

--- 631-01.js
+++ 631-01.js
@@ -1,14 +1,31 @@
 https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
 
-function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   // code goes here
-  return laLigaGoals+copaDelReyGoals+championsLeagueGoals;
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
-
 
 https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
 
-function move (position, roll) {
+function move(position, roll) {
   // return the new position
-  return position+roll*2
+  return position + roll * 2;
+}
+
+
+https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+
+function litres(time) {
+  return Math.floor(0.5 * time);
+}
+
+
+https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+
+function greet(name, owner) {
+  if (name === owner) {
+    return "Hello boss";
+  } else {
+    return "Hello guest";
+  }
 }
