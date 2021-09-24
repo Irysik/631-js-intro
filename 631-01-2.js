6765a3699020bb285d293232238f1b6705c0bac4
Irysik <89611587+Irysik@users.noreply.github.com>
2021-09-24 11:55:19 +0300

    Create 631-01-2.js

---
+++ 631-01-2.js
@@ -0,0 +1,13 @@
+https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+
+function DNAtoRNA(dna) {
+  let rna = dna.split("T").join("U");
+  return rna;
+}
+
+https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+
+function stringToArray(string) {
+  var str = string.split(" ");
+  return str;
+}
