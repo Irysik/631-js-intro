e04402152cb2e3c9ca04b46ad72127e55aeb9611
Irysik <89611587+Irysik@users.noreply.github.com>
2021-09-25 23:39:40 +0300

    Update 631-01-3.js

--- 631-01-3.js
+++ 631-01-3.js
@@ -5,3 +5,11 @@ class SmallestIntegerFinder {
     return Math.min(...args);
   }
 }
+
+
+https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+
+function circleCircumference(circle) {
+  let num = 3.14159265358 * 2 * circle.radius;
+  return num;
+}
