7212817633bffb16c5dbaa8f9076ecffc13757c3
Irysik <89611587+Irysik@users.noreply.github.com>
2021-09-26 00:34:34 +0300

    Update 631-01-3.js

--- 631-01-3.js
+++ 631-01-3.js
@@ -13,3 +13,14 @@ function circleCircumference(circle) {
   let num = 3.14159265358 * 2 * circle.radius;
   return num;
 }
+
+https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+
+function giveMeFive(obj) {
+  const five = [];
+  for (let key in obj) {
+    if (key.length === 5) five.push(key);
+    if (obj[key].length === 5) five.push(obj[key]);
+  }
+  return five;
+}
