3b51d1a4412528376794b445e6490b897d3baa34
Irysik <89611587+Irysik@users.noreply.github.com>
2021-09-25 21:59:22 +0300

    Create 631-01-3.js

---
+++ 631-01-3.js
@@ -0,0 +1,7 @@
+https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args);
+  }
+}
